8e58cfedd5be0af60b6f43879b3dfdc405a2c9a5
John-Hawker <JHawker@uvu.edu>
2023-08-15 10:04:43 -0600

    updating the_book folder

---
+++ the_book/projects/hello_cargo/target/debug/deps/hello_cargo-6fce5f84c876589c.d
@@ -0,0 +1,5 @@
+/home/j/CS339R-Fall-2023/the_book/projects/hello_cargo/target/debug/deps/hello_cargo-6fce5f84c876589c.rmeta: src/main.rs
+
+/home/j/CS339R-Fall-2023/the_book/projects/hello_cargo/target/debug/deps/hello_cargo-6fce5f84c876589c.d: src/main.rs
+
+src/main.rs:
---
+++ the_book/projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/j/CS339R-Fall-2023/the_book/projects/hello_cargo/target/debug/hello_cargo: /home/j/CS339R-Fall-2023/the_book/projects/hello_cargo/src/main.rs
